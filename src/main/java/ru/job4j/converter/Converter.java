b5e0bcad79062748df3e9fc24df1a7e689c05798
RuslanZb <zubairov.rr@mail.ru>
2021-09-26 12:00:32 +0300

    1. Модульные тесты. [#159435 #208353]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -13,10 +13,17 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(180);
-        System.out.println("140 rubles are " + dollar + " dollars.");
+        float in1 = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in1);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+        float in2 = 180;
+        expected = 3;
+        out = Converter.rubleToDollar(in2);
+        passed = expected == out;
+        System.out.println("180 rubles are 3 dollars. Test result : " + passed);
     }
 }
+
 
