2cd13c7a487856a268ceeca313d487d804940394
RuslanZb <zubairov.rr@mail.ru>
2022-06-07 23:58:34 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,5 +1,10 @@
 package ru.job4j.array;
 
+import java.io.FileOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] array = new int[size][size];
@@ -9,5 +14,16 @@ public class Matrix {
             }
         }
         return array;
+    }
+
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("matrix.txt")) {
+            for (int[] array : Matrix.multiple(9)) {
+                out.write(Arrays.toString(array).getBytes());
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
